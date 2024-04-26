750c814a9cca80574855ce731ae410c810817180
TSgt Jason Leaver <jason.leaver@us.af.mil>
2024-04-26 04:57:03 -0500

    house keeping 🧹

--- nzthermo/_c.pyi
+++ nzthermo/_c.pyi
@@ -5,18 +5,18 @@ import numpy as np
 from ._typing import Kelvin, Pascal, N, Z, shape
 
 _T = TypeVar("_T")
-_dtype_T = TypeVar("_dtype_T", np.float64, np.float32)
-_dtype = type[_dtype_T] | type[float] | L["float32", "float64"]
+_dtype_T = TypeVar("_dtype_T", bound=np.float_)
+
 OPENMP_ENABLED: bool
 
 @overload
 def moist_lapse(
     pressure: Pascal[np.ndarray[shape[N], np.dtype[_dtype_T]]],
-    temperature: Kelvin[np.ndarray[shape[N], np.dtype[_dtype_T]]],
-    reference_pressure: Pascal[np.ndarray[shape[N], np.dtype[_dtype_T]]] = ...,
+    temperature: Kelvin[np.ndarray[shape[N], np.dtype[np.float_]]],
+    reference_pressure: Pascal[np.ndarray[shape[N], np.dtype[np.float_]]] = ...,
     *,
     step: float = ...,
-    dtype: _dtype[_dtype_T] | None = ...,
+    dtype: type[_dtype_T] | type[float] | L["float32", "float64"] | None = ...,
 ) -> Kelvin[np.ndarray[shape[N], np.dtype[_dtype_T]]]: ...
 @overload
 def moist_lapse(
@@ -25,28 +25,29 @@ def moist_lapse(
         | np.ndarray[shape[L[1], Z], np.dtype[_dtype_T]]
         | np.ndarray[shape[N, Z], np.dtype[_dtype_T]]
     ],
-    temperature: Kelvin[np.ndarray[shape[N], np.dtype[_dtype_T]]],
-    reference_pressure: Pascal[np.ndarray[N, np.dtype[_dtype_T]]] | None = ...,
+    temperature: Kelvin[np.ndarray[shape[N], np.dtype[np.float_]]],
+    reference_pressure: Pascal[np.ndarray[N, np.dtype[np.float_]]] | None = ...,
     *,
     step: float = ...,
-    dtype: _dtype[_dtype_T] | None = ...,
+    dtype: type[_dtype_T] | type[float] | L["float32", "float64"] | None = ...,
 ) -> Kelvin[np.ndarray[shape[N, Z], np.dtype[_dtype_T]]]: ...
+@overload
 def moist_lapse(
     pressure: Pascal[np.ndarray[Any, np.dtype[_dtype_T]]],
-    temperature: Kelvin[np.ndarray[Any, np.dtype[_dtype_T]]],
-    reference_pressure: Pascal[np.ndarray[Any, np.dtype[_dtype_T]]] | None = ...,
+    temperature: Kelvin[np.ndarray[Any, np.dtype[np.float_]]],
+    reference_pressure: Pascal[np.ndarray[Any, np.dtype[np.float_]]] | None = ...,
     *,
     step: float = ...,
-    dtype: _dtype[_dtype_T] | None = ...,
-) -> Kelvin[np.ndarray[shape[N, Z], np.dtype[_dtype_T]]]: ...
+    dtype: type[_dtype_T] | type[float] | L["float32", "float64"] | None = ...,
+) -> Kelvin[np.ndarray[Any, np.dtype[_dtype_T]]]: ...
 def lcl(
     pressure: Pascal[np.ndarray[shape[N], np.dtype[_dtype_T]]],
-    temperature: Kelvin[np.ndarray[shape[N], np.dtype[_dtype_T]]],
-    dewpoint: Kelvin[np.ndarray[shape[N], np.dtype[_dtype_T]]],
+    temperature: Kelvin[np.ndarray[shape[N], np.dtype[np.float_]]],
+    dewpoint: Kelvin[np.ndarray[shape[N], np.dtype[np.float_]]],
     *,
     max_iters: int = ...,
     tolerance: float = ...,
-    dtype: _dtype[_dtype_T] | None = ...,
+    dtype: type[_dtype_T] | type[float] | L["float32", "float64"] | None = ...,
 ) -> tuple[
     Pascal[np.ndarray[shape[N], np.dtype[_dtype_T]]],
     Kelvin[np.ndarray[shape[N], np.dtype[_dtype_T]]],
